07792ad75f9ec6d15c6452905c5f82b9be9b3860
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-11-01 09:58:31 +0500

    WIP

--- bot.js
+++ bot.js
@@ -2,11 +2,16 @@ const Telegraf = require('telegraf');
 
 const bot = new Telegraf('2083322429:AAF3iTILrFaa2Okzh-FhexVsCf-tB92kHIg');
 
+bot.on('message', ctx => {
+    console.log(ctx.message.photo);
+});
+
 bot.command('test', ctx => {
     // url
     bot.telegram.sendPhoto(ctx.chat.id, "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/1200px-Above_Gotham.jpg");
 
     // file path
+    bot.telegram.sendPhoto(ctx.chat.id, {source: "./res/dubai.jpeg"});
 });
 
 bot.launch();
